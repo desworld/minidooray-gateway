b18f0f16c42382c3487baad17931b2a6ddb5c461
Yongtaek Lee <itaekit@gmail.com>
2024-10-13 19:36:13 +0900

    Project 등록

--- src/main/java/com/itaekit/gateway/controller/TaskController.java
+++ src/main/java/com/itaekit/gateway/controller/TaskController.java
@@ -1,4 +1,34 @@
 package com.itaekit.gateway.controller;
 
+import com.itaekit.gateway.dto.project.request.CreateProjectRequestDto;
+import com.itaekit.gateway.dto.project.response.CreateProjectResponseDto;
+import com.itaekit.gateway.dto.user.UserDto;
+import com.itaekit.gateway.service.AccountService;
+import com.itaekit.gateway.service.TaskService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class TaskController {
+    private final TaskService taskService;
+    private final AccountService accountService;
+
+    @GetMapping("/project/register")
+    public String registerProject(HttpSession session, Model model) {
+        UserDto userDto = accountService.getUserDetailsByUserId(session.getAttribute("userId").toString());
+        model.addAttribute("user", userDto);
+        return "projectRegisterForm";
+    }
+
+    @PostMapping("/project/register")
+    public String doRegisterProject(CreateProjectRequestDto requestDto) {
+        CreateProjectResponseDto responseDto = taskService.registerProject(requestDto);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/itaekit/gateway/service/TaskService.java
+++ src/main/java/com/itaekit/gateway/service/TaskService.java
@@ -1,5 +1,8 @@
 package com.itaekit.gateway.service;
 
-public interface TaskService {
+import com.itaekit.gateway.dto.project.request.CreateProjectRequestDto;
+import com.itaekit.gateway.dto.project.response.CreateProjectResponseDto;
 
+public interface TaskService {
+    CreateProjectResponseDto registerProject(CreateProjectRequestDto requestDto);
 }
